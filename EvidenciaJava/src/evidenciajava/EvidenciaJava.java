3214850cd62d371b9b8ae46e08794118a3a6f067
Alan Franco <alanfrancoj@gmail.com>
2021-12-02 19:03:43 -0600

    Opciones para Citas agregadas

--- EvidenciaJava/src/evidenciajava/EvidenciaJava.java
+++ EvidenciaJava/src/evidenciajava/EvidenciaJava.java
@@ -52,7 +52,7 @@ public class EvidenciaJava {
                     while(!volver){
                         System.out.println("---DOCTORES---\n");
                         System.out.println("Elija una opcion:");
-                        System.out.println("1.Imprimir lista de doctores");
+                        System.out.println("1. Imprimir lista de doctores");
                         System.out.println("2. Agregar doctor");
                         System.out.println("3. Eliminar doctor");
                         System.out.println("0. Volver");
@@ -131,7 +131,7 @@ public class EvidenciaJava {
                     while(!volver){
                         System.out.println("---PACIENTES---\n");
                         System.out.println("Elija una opcion:");
-                        System.out.println("1.Imprimir lista de pacientes");
+                        System.out.println("1. Imprimir lista de pacientes");
                         System.out.println("2. Agregar paciente");
                         System.out.println("3. Eliminar paciente");
                         System.out.println("0. Volver");
@@ -184,6 +184,109 @@ public class EvidenciaJava {
                                 }
                                 if(!existe){
                                     System.out.println("No existe un paciente con ese ID");
+                                }
+                                continuar();
+                                break;
+                            }
+                            
+                            case 0:{
+                                volver = true;
+                                break;
+                            }
+                            
+                            default:{
+                                System.out.println("Ingrese una opcion valida, por favor");
+                                continuar();
+                            }
+                        }
+                    }
+                    break;
+                }
+                
+                case 3:{
+                    volver = false;
+                    while(!volver){
+                        System.out.println("---CITAS---\n");
+                        System.out.println("Elija una opcion:");
+                        System.out.println("1. Imprimir citas existentes");
+                        System.out.println("2. Agregar cita");
+                        System.out.println("3. Eliminar cita");
+                        System.out.println("0. Volver");
+                        try{
+                            opcion2 = Integer.parseInt(br.readLine());
+                        }catch(Exception e){
+                            opcion2 = -1;
+                            System.out.println("Opcion no valida ->" + e);
+                        }
+                        switch(opcion2){
+                            case 1:{
+                                for (int i = 0; i < citas.size(); i++) {
+                                    System.out.println(citas.get(i));
+                                }
+                                continuar();
+                                break;
+                            }
+                            
+                            case 2:{
+                                System.out.println("Ingrese ID de la nueva cita");
+                                String id = br.readLine();
+                                boolean existe = false;
+                                for (int i = 0; i < citas.size(); i++) {
+                                    if(citas.get(i).getId().equals(id))
+                                        existe = true;
+                                }
+                                if(existe){
+                                    System.out.println("Ya existe una cita con ese ID");
+                                }else{
+                                    System.out.println("Ingrese el motivo de la cita");
+                                    String mot = br.readLine();
+                                    System.out.println("Ingrese la fecha de la cita");
+                                    String fecha = br.readLine();
+                                    System.out.println("Ingrese el ID del doctor");
+                                    String iddoc = br.readLine();
+                                    boolean existedoc = false;
+                                    int indexdoc = -1;
+                                    for (int i = 0; i < doctores.size(); i++) {
+                                        if(doctores.get(i).getId().equals(iddoc)){
+                                            existedoc = true;
+                                            indexdoc = i;
+                                        } 
+                                    }
+                                    System.out.println("Ingrese el ID del paciente");
+                                    String idpac = br.readLine();
+                                    boolean existepac = false;
+                                    int indexpac = -1;
+                                    for (int i = 0; i < pacientes.size(); i++) {
+                                        if(pacientes.get(i).getId().equals(idpac)){
+                                            existepac = true;
+                                            indexpac = i;
+                                        } 
+                                    }
+                                    if(existedoc && existepac){
+                                        citas.add(new Cita(id, mot, fecha, doctores.get(indexdoc), pacientes.get(indexpac)));
+                                        System.out.println("Nueva cita agregada correctamente");
+                                    }else{
+                                        System.out.println("Error: ID de doctor o paciente no validos");
+                                    }
+                                }
+                                continuar();
+                                break;
+                            }
+                            
+                            case 3:{
+                                System.out.println("Ingrese ID de la cita a eliminar");
+                                String id = br.readLine();
+                                boolean existe = false;
+                                for (int i = 0; i < citas.size(); i++) {
+                                    if(citas.get(i).getId().equals(id)){
+                                        existe = true;
+                                        citas.remove(i);
+                                        System.out.println("Cita eliminada correctamente");
+                                        break;
+                                    }
+                                }
+                                if(!existe){
+                                    System.out.println("No existe una cita con ese ID");
                                 }
                                 continuar();
                                 break;
